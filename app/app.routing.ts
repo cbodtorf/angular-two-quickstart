c3b6d419aa72f00e06cbb4762069a2b79e966d38
Cbodtorf <cbodtorf@gmail.com>
2016-09-29 19:15:46 -0400

    fixed some spacing, made it pretty.

--- app/app.routing.ts
+++ app/app.routing.ts
@@ -2,7 +2,7 @@ import { ModuleWithProviders }    from '@angular/core'
 import { Routes, RouterModule }   from '@angular/router'
 
 import { HeroesComponent }        from './heroes.component'
-import { HeroDetailComponent }        from './hero-detail.component'
+import { HeroDetailComponent }    from './hero-detail.component'
 import { DashboardComponent }     from './dashboard.component'
 
 
