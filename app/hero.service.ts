869186d7ecf36a56054bedb418f211a818a85cb1
Cbodtorf <cbodtorf@gmail.com>
2016-09-29 19:39:56 -0400

    made some changes to the hero.service since we aren't using the mock file and more. We've set up up to work with our http methods. We have expanded our get heroes method to make the GET request at url: app/heroes. Also added an error handler to the Hero Service.

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -1,14 +1,28 @@
-import { Injectable } from '@angular/core'
+import { Injectable }    from '@angular/core'
+import { Headers, Http } from '@angular/http'
 
-import { Hero }       from './hero'
-import { HEROES }     from './mock-heroes'
+import 'rxjs/add/operator/toPromise';
+
+import { Hero }          from './hero'
+
 
 
 @Injectable()
 export class HeroService {
+  private heroesUrl = 'app/heroes' // URL to web api
 
-  getHeroes(): Promise<Hero[]> { // stub w/ 'void {}' if nothing there
-    return Promise.resolve(HEROES);
+  private handleError(error: any): Promise<any> {
+    console.error('An error occured', error) // for demo purposes only
+    return Promise.reject(error.message || error);
+  }
+
+  constructor(private http: Http) { }
+
+  getHeroes(): Promise<Hero[]> {
+    return this.http.get(this.heroesUrl)
+               .toPromise()
+               .then(response => response.json().data as Hero[])
+               .catch(this.handleError);
   }
 
   getHeroesSlowly(): Promise<Hero[]> {
